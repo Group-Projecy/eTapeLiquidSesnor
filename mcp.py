589b38e20d9ca743d1d16ef490575dfe4360a690
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-09 21:20:45 +0000

    MCP tested and working - Improvements needed for when connecting the etape

--- mcp.py
+++ mcp.py
@@ -1,9 +1,13 @@
 #getting the mcp to convert analog to digital first as the the etape outputs analog
 #then put both codes together
+
+
 from gpiozero import MCP3008
 
 pot = MCP3008(0)
 
 while True:
     print(pot.value)
+    #TODO: Slow the output down - printed to fast
+    #TODO: Code the etape with the slowed down out and intergrate the mcp and etape as one
 
