e08acbc8505e22287b15e92e93279c6bcdf3d533
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-09 20:08:00 +0000

    Testing MCP can read output with LDR - Light Dependent Resistor

--- mcp.py
+++ mcp.py
@@ -1,2 +1,9 @@
 #getting the mcp to convert analog to digital first as the the etape outputs analog
 #then put both codes together
+from gpiozero import MCP3008
+
+pot = MCP3008(0)
+
+while True:
+    print(pot.value)
+
