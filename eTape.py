1c02e7b9b998f1ba80a16d5011a263348015a498
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-10 23:29:44 +0000

    prints in loop to debug - debugging sesnor output

--- eTape.py
+++ eTape.py
@@ -1,4 +1,3 @@
-# https://learn.adafruit.com/smart-measuring-cup?view=all - Helped
 from gpiozero import MCP3008
 import time
 
@@ -6,19 +5,25 @@ resistor = 560
 
 eTape = MCP3008(0)
 
-
+# eTape.voltage
 def readVoltage(eTape):
     #getting the ADC value
     adcValue = eTape.value
     # covert adcValue (analogValue) to a voltage (0-5V)
-    voltage = adcValue * (5.0/1023)
+    # voltage = adcValue * (5.0/1023)
+    voltage = (adcValue/1023)*5.0
     return voltage
 
 
 while True:
-    print(f'ADC Value: {eTape.value}')
-    voltage = readVoltage(eTape)
-    print(f'Voltage: {voltage}')
+    # print(f'ADC Value: {eTape.value}')
+    # voltage = readVoltage(eTape)
+    # print(f'Voltage: {voltage}')
+    analogReading = eTape.value
+    print(f'Analog Reading: {analogReading}')
+    # covert value to resistance
+    resistance = resistor / analogReading
+    print(f'Sensor output Resistance: {resistance}')
     time.sleep(5)
 
 
