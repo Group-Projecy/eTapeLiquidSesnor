b89d1304a98b08b9b777d28966a58392a94d5a4c
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-11 01:09:26 +0000

    Sensor code working - next step is to calibrate the sensor

--- eTape.py
+++ eTape.py
@@ -16,15 +16,29 @@ def readVoltage(eTape):
 
 
 while True:
-    # print(f'ADC Value: {eTape.value}')
-    # voltage = readVoltage(eTape)
-    # print(f'Voltage: {voltage}')
-    analogReading = eTape.value
-    print(f'Analog Reading: {analogReading}')
-    # covert value to resistance
-    resistance = resistor / analogReading
-    print(f'Sensor output Resistance: {resistance}')
+    # # print(f'ADC Value: {eTape.value}')
+    # # voltage = readVoltage(eTape)
+    # # print(f'Voltage: {voltage}')
+    # analogReading = eTape.value
+    # print(f'Analog Reading: {analogReading}')
+    # # covert value to resistance
+    # # resistance = (1023.0 / analogReading) - 1.0
+    # resistance = resistor / analogReading
+    # print(f'Sensor output Resistance: {resistance} ohms')
+    # volts = 5 * analogReading
+    # print(f'volt {volts}')
+    # print('\n')
+    # time.sleep(5)
+
+    adcReading = eTape.value
+    print(f'ADC Reading: {adcReading}')
+    volts = 5 * adcReading
+    x = (1023.0 / 5.0) * volts
+    print(f'x {x}')
+    print('\n')
     time.sleep(5)
+
+
 
 
 
