c9432355da77fa924e508e56a9dbeb7314dff3ff
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-11 15:19:12 +0000

    Sensor code to calibrate written

--- eTape.py
+++ eTape.py
@@ -2,28 +2,36 @@ from gpiozero import MCP3008
 import time
 
 resistor = 560
-
 eTape = MCP3008(0)
 
+no_volume_resistance = 806.546  #Resistance value (in ohms) when no liquid is present
+calibration_resistance = 463.582 #Resistance value (in ohms) when liquid is at max line.
 
-def readResistance(eTape,resistor):
-    return 0
+
+def ohmsValue(eTape):
+    analogReading = eTape.value
+    # covert value from kohms  to ohms
+    ohmsValue = analogReading * 1000
+    return ohmsValue
+
+def waterLevelPrentage(ohmsValue):
+    precentage = (no_volume_resistance - ohmsValue) / (no_volume_resistance - calibration_resistance)
+    cm = 30 * precentage
+    print(f'cm value: {cm}')
+    return precentage
+
+
+
 
 
 while True:
-    analogReading = eTape.value
-    print(f'Analog Reading: {analogReading}')
-    # covert value from kohms  to ohms
-    ohmsValue = analogReading * 1000
-    print(f'ohms Vale {ohmsValue}')
-    voltage = (5.0 / 1023) * ohmsValue
-    print(f'voltage: {voltage}')
+    ohmsValue = ohmsValue(eTape)
+
     print('\n')
     time.sleep(2)
 
 
-# TODO double check code
-# TODO my code is out putting kohms need to change to ohms
+
 # https://hextobinary.com/unit/resistance/from/kohm/to/ohm
 
 
