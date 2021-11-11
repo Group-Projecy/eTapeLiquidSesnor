dea2c489597e1e8a28e09d96da6430f3d6ab0fc6
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-11 20:27:22 +0000

    Main method added

--- eTape.py
+++ eTape.py
@@ -3,29 +3,45 @@ import time
 
 resistor = 560
 eTape = MCP3008(0)
-
+# TODO: Calibration is off need to re adjust
 no_volume_resistance = 806.546  # Resistance value (in ohms) when no liquid is present
 calibration_resistance = 463.582  # Resistance value (in ohms) when liquid is at max line.
 
 
-def ohmsValue(eTape):
-    analogReading = eTape.value
-    # covert value from kohms  to ohms
-    ohmsValue = analogReading * 1000
-    return ohmsValue
+def main():
+    while True:
+        # ohms_value = calculate_ohms_value(eTape)
+        # water_level = water_level_prentage(ohms_value)
+        # print(f'water level %: {water_level}')
+        # print('\n')
+        test_code()
+        time.sleep(1)
 
 
-def waterLevelPrentage(ohmsValue):
-    precentage = (no_volume_resistance - ohmsValue) / (no_volume_resistance - calibration_resistance)
-    cm = 30 * precentage
+def read_resistance():
+    reading = eTape.value * 1000
+    # covert to resistance
+    resist = (1023 / reading) - 1
+    resist = resistor / resist
+    print(f'resistance: {resist}')
+
+
+def water_level_prentage(ohms_value):
+    percentage = (no_volume_resistance - ohms_value) / (no_volume_resistance - calibration_resistance)
+    cm = 30 * percentage
     print(f'cm value: {cm}')
-    return precentage
+    return percentage
 
 
-while True:
-    ohmsValue = ohmsValue(eTape)
-    waterLevel = waterLevelPrentage(ohmsValue)
-    print(f'water level %: {waterLevel}')
-    print('\n')
-    time.sleep(2)
+def test_code():
+    reading = eTape.value * 1000
+    print(f'adc: {reading}')
+    # covert to resistance
+    resist = (1023 / reading) - 1
+    resist = resistor / resist
+    print(f'resistance: {resist}')
+    water_level_prentage(reading)
 
+
+if __name__ == "__main__":
+    main()
