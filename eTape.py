d640021e8cfb374c9f94fc5051a08bbc9a00839d
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-23 11:49:51 +0000

    Pubnub coded so sensor can post to channel

--- eTape.py
+++ eTape.py
@@ -21,6 +21,7 @@ pnconfig.publish_key = os.getenv("PUBNUB_PUBLISH")
 pnconfig.uuid = '2ca147c6-d6e1-4d2c-9c38-a34d6938efd6'
 pubnub = PubNub(pnconfig)
 
+
 def main():
     while True:
         test_code()
@@ -43,7 +44,7 @@ def read_resistance():
     return resist
 
 
-def water_level(ohms_value):
+def get_water_level(ohms_value):
     if ohms_value > no_volume_resistance or (no_volume_resistance - calibration_resistance) == 0.0:
         # if no max value for resistance is set
         return 0.0
@@ -61,10 +62,78 @@ def test_code():
     # ohms_value = resistance
     resistance = read_resistance()
     print("====================")
-    cm_level = water_level(resistance)
-    print(f'{cm_level} cm')
-    print('WaterLevel: {0:0.1f} cm'.format(cm_level))
-    print('WaterLevel: {0:0.0f} cm'.format(cm_level))
+    water_level = get_water_level(resistance)
+    print(f'{water_level} cm')
+    print('WaterLevel: {0:0.1f} cm'.format(water_level))
+    print('WaterLevel: {0:0.0f} cm'.format(water_level))
+    publish(my_channel, {"WaterLevel ": '{0:0.0f} cm'.format()})
+    publish(my_channel, {"WaterLevel ": '{0:0.1f} cm'.format()})
+
+
+# ----------------------------- PubNub Code ---------------------------------------------
+
+def publish(channel, msg):
+    pubnub.publish().channel(my_channel).message(msg).pn_async(my_publish_callback)
+
+
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            pubnub.publish().channel(my_channel).message('Hello world!').pn_async(my_publish_callback)
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+
+    def message(self, message):
+        # Handle new message stored in message.message
+        try:
+            print(message.message, ": ", type(message.message))
+            msg = message.message
+            key = list(msg.keys())
+            if key[0] == "event":
+                self.handleEvent(msg)
+        except Exception as e:
+            print("Received: ", message.message)
+            print(e)
+            pass
+    #  handle event = if oil level = < 20% send message
+    # def handleEvent(self, msg):
+    #     global data
+    #     eventData = msg["event"]
+    #     key = list(eventData.keys())
+    #     print(key)
+    #     print(key[0])
+    #     if key[0] in sensor_list:
+    #         if eventData[key[0]] is True:
+    #             print("Setting the alarm")
+    #             data["alarm"] = True
+    #         elif eventData[key[0]] is False:
+    #             print("Turning alarm off")
+    #             data["alarm"] = False
+
+
+def my_publish_callback(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass  # Message successfully published to specified channel.
+    else:
+        pass  # Handle message publish error. Check 'category' property to find out possible issue
+        # because of which request did fail.
+        # Request can be resent using: [status retry];
 
 
 if __name__ == "__main__":
